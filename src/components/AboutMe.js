13e4dc41ad716a3d733f0d3f5384e50d0e058078
jeacqueline.rios-srwity <jeacqueline.rios@outlook.com>
2023-05-26 00:59:04 -0600

    add images to readme, edited styles, fix aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,9 @@ const AboutMe = () => {
     <div>
       <div className="about-container">
         <div className="about-content">
-          <p>Welcome to my portfolio! I'm Jeacqueline, a new programmer with experience in HTML, CSS, JavaScript, React, Node.js, Express.js, MongoDB, MySQL, Python. 🤓</p>
+          <p>Welcome to my portfolio! I'm Jeacqueline, a new programmer</p>
+          <p>Recently, I have expanded my skill set by acquiring knowledge in GitHub, JavaScript, HTML, CSS, and Node.js. I am constantly learning and staying up-to-date with the latest technologies and best practices in the industry. I am a fast self-learner, always eager to explore new tools and frameworks that can enhance my development capabilities.</p>
+          <p>I have also obtained certifications in Scrum, Design Thinking, and Python, further strengthening my project management expertise and technical knowledge. These certifications have equipped me with valuable insights into agile methodologies, user-centered design, and programming fundamentals.</p>
         </div>
         <div className="social-links">
           <a href="https://github.com/Jeacqueline" target="_blank" rel="noopener noreferrer">
